1d162dab799254786c0ee20d03aa2217e477a2a7
Monty <maestroguiao@gmail.com>
2021-09-21 01:37:28 +0800

    small fix

--- scripts/TypeSafety.java
+++ scripts/TypeSafety.java
@@ -18,11 +18,16 @@ public class TypeSafety extends Deck {
     }
 
     public static boolean Take(int Player, String Input) {
-        int length = Players[Player].length;
-        int index = (Integer.parseInt(Input.split(" ")[1])) - 1;
+        String[] Type = Input.split(" ");
 
-        if(index < length) {
-            return true;
+        if(Type.length == 2) {
+            int length = Players[Player].length;
+            int index = (Integer.parseInt(Input.split(" ")[1])) - 1;
+
+            if(index < length) {
+                return true;
+            }
+            else return false;
         }
         else return false;
     }
